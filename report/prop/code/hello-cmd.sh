a8b29448b775e9645fed75e2c0bfe22d52c72b40
mohan <mohankumar@gatech.edu>
2018-04-28 12:28:20 -0400

    x

--- report/prop/code/hello-cmd.sh
+++
@@ -1,8 +0,0 @@
-$ opensgx -k
-> generate sign.key
-$ opensgx -c hello.c
-> generate hello.sgx executable
-$ opensgx -s hello.sgx --key sign.key
-> generate hello.conf
-$ opensgx hello.sgx hello.conf
-hello sgx!
--- report/prop/code/hello.c
+++
@@ -1,9 +0,0 @@
-/* hello.c */
-#include <sgx-lib.h>
-
-void enclave_main()
-{
-    char *hello = "hello sgx!\n";
-    sgx_enclave_write(hello, sgx_strlen(hello));
-    sgx_exit(NULL);
-}
